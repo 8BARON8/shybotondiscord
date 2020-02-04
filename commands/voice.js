1136eb8b7490bb19b62a34673c8b816407ed824b
Dmitriy <48184403+8BARON8@users.noreply.github.com>
2020-02-04 23:36:30 +0300

    Update voice.js

--- commands/voice.js
+++ commands/voice.js
@@ -1,11 +1,11 @@
 module.exports.run = async (bot,message,args)  => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`${message.author.username}, приветствует вас! 🔊<@&669637548989087769>, мне скучно. Залетайте поговорим)`.then(msg => msg.delete(15*1000))
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`${message.author.username}, приветствует вас! 🔊<@&669637548989087769>, мне скучно. Залетайте поговорим`).then(msg => msg.delete(15*1000))
     let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
     let argsUser
     if (member) argsUser = member.user
     else argsUser = message.author
     let content = args.slice(0).join(' ')
-    if (!content) return message.channel.send(`<@!${argsUser.id}>, приветствует вас! 🔊<@&669637548989087769>, мне скучно. Залетайте поговорим)`.then(msg => msg.delete(15*1000))
+    if (!content) return message.channel.send(`<@!${argsUser.id}>, приветствует вас! 🔊<@&669637548989087769>, мне скучно. Залетайте поговорим`).then(msg => msg.delete(15*1000))
     await message.channel.send(content)
     await message.delete()
 }
